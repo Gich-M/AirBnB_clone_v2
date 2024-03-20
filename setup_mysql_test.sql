dfd40124c13a4f3af8d1afaaf1f9fc6f35856b2b
Gich-M <gichmuriuki21@gmail.com>
2024-03-20 15:51:26 -0700

    sql_files

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,14 +1,5 @@
--- Create the database
+-- Prepares a MySQL Test server for the project.
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-
--- Create the user
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-
---Grant privileges on the database to the user
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-
---Grant SELECT privileges on the database performance_schema to the user
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
-
--- Flush privilege to reload the grant table
-FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON hbnb_test_db . * TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema . * TO 'hbnb_test'@'localhost';
